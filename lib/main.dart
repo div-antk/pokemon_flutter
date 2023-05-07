5a03d32f63ed235f2692ae9e63160636d0ce21af
div-antk <51283468+owaii@users.noreply.github.com>
2023-05-07 23:59:00 +0900

    属性タグの実装

--- lib/main.dart
+++ lib/main.dart
@@ -21,27 +21,28 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-              height: 200,
-              width: 200,
-            ),
-            const Text(
-              'pikachu',
-              style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-          ])
-      ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Image.network(
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+          height: 200,
+          width: 200,
+        ),
+        const Text(
+          'pikachu',
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              color: Colors.yellow, borderRadius: BorderRadius.circular(20)),
+          child: const Text('electric'),
+        )
+      ])),
     );
   }
 }
