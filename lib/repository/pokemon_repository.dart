65675d0f572698c6db937829c40b58807b2495e8
div-antk <1984ta@gmail.com>
2023-07-28 23:59:00 +0900

    ポケモン一覧を取得する関数を追加

--- lib/repository/pokemon_repository.dart
+++ lib/repository/pokemon_repository.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:pokemon_flutter/api/api_client.dart';
+import 'package:pokemon_flutter/entity/pokemon/pokemon.dart';
 import 'package:pokemon_flutter/entity/pokemon_detail/pokemon_detail.dart';
 
 class PokemonRepository {
@@ -16,6 +19,13 @@ class PokemonRepository {
         }
       }),
     );
+  }
+
+  Future<PokemonList> fetchItems() async {
+    final client = RestClient(dio);
+    final response = await client.fetchPokemons();
+
+    return response;
   }
 
   Future<PokemonDetail> fetchItemDetail(String id) async {
