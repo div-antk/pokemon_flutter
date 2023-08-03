e17b155ff49af5a7bb8e3b36c0a7e9f446272e77
div-antk <1984ta@gmail.com>
2023-08-03 23:59:00 +0900

    メインページの作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_flutter/view/abstract_widget.dart';
+import 'package:pokemon_flutter/view/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Image.network(
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-          height: 200,
-          width: 200,
-        ),
-        const Text(
-          'pikachu',
-          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-              color: Colors.yellow, borderRadius: BorderRadius.circular(20)),
-          child: const Text('electric'),
-        )
-      ])),
-    );
-  }
+  runApp(
+    const ProviderScope(
+      child: AppRoute(),
+    ),
+  );
 }
